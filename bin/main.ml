a7c3e8ede7f33c08cd0c207d1c10f8010beb1d86
rimpo <manojbisht5@gmail.com>
2024-01-21 17:05:33 +0100

    fix

--- bin/main.ml
+++ bin/main.ml
@@ -1,32 +1,44 @@
 open Board
 
-let cell_width = 5
-let size = 128
-let colors v =  
-  match v with 
-    | Dead -> Raylib.Color.gray
-    | Alive -> Raylib.Color.red
+(* cell width is used to draw the size of each cell *)
+let cell_width = 3
+
+(* size is the square width of the board *)
+let size = 256
+
+let colors v =
+  match v with
+  | Dead -> Raylib.Color.gray
+  | Alive -> Raylib.Color.red
+;;
 
 let setup size =
-  Raylib.init_window ((size * cell_width) + 100) ((size * cell_width) + 100) "game of life";
+  Raylib.init_window ((size * cell_width) + 80) ((size * cell_width) + 80) "game of life";
   Raylib.set_target_fps 10;
-  let b = Array.make (size * size) Dead in 
-    mark_alive b size (form_list size); 
-    b
+  let b = Array.make (size * size) Dead in
+  mark_alive b size (form_list size);
+  b
+;;
 
 let rec loop b size =
-  if Raylib.window_should_close () then Raylib.close_window ()
+  if Raylib.window_should_close ()
+  then Raylib.close_window ()
   else
     let open Raylib in
     begin_drawing ();
     clear_background Color.darkgray;
-      for i = 0 to (size * size)-1 do
-        let (x, y) = get_position i size in
-          draw_rectangle (x*cell_width+50) (y*cell_width+50) cell_width cell_width (colors b.(i))  
-      done;
-
+    for i = 0 to (size * size) - 1 do
+      let x, y = get_position i size in
+      draw_rectangle
+        ((x * cell_width) + 50)
+        ((y * cell_width) + 50)
+        cell_width
+        cell_width
+        (colors b.(i))
+    done;
     next b size;
     end_drawing ();
     loop b size
+;;
 
-let () =  loop (setup size) size
+let () = loop (setup size) size
--- lib/board.ml
+++ lib/board.ml
@@ -1,78 +1,94 @@
 type status =
   | Dead
-  | Alive 
+  | Alive
 
 (* type position = int * int *)
 
-let get_index pos size = (snd pos) * size + (fst pos) 
+let get_index pos size = (snd pos * size) + fst pos
+let get_position idx size = idx mod size, idx / size
 
-let get_position idx size = (idx mod size, idx / size)
-
-
-let is_alive idx b = 
-    match b.(idx) with
-      | Alive -> true
-      | Dead -> false
-
-
-(* count for each cell which are alive *)
-let count_if_alive b x y size = 
-  let i = get_index (x, y) size in
-    match i with
-    | x when -1 < x && x < (size * size)  -> if is_alive x b then 1 else 0
-    | _ -> 0
-
-let get_position_coordinate x y = 
-  [(x-1, y-1); (x, y-1); (x+1, y-1);(x-1, y); (*(x, y);*) (x+1, y);(x-1, y+1); (x, y+1); (x+1,y+1)]
-
-
-let count_neighbours_cell b x y size =
-  let rec count_neighbours_cell_aux acc b lst size = 
-      match lst with
-        | [] -> acc
-        | (x, y) :: tl -> count_neighbours_cell_aux (acc+(count_if_alive b x y size)) b tl size
-  in count_neighbours_cell_aux 0 b (get_position_coordinate x y) size
-
-
-
-let count_neighbours b i size= 
-    let (x, y) = get_position i size in
-    count_neighbours_cell b x y size
-(* count the neighbours each cell has. edges and corners will have less neighbours *)
-let neighbours b size = 
-    let n = Array.make (size * size) 0 in
-      for i = 0 to (size * size) - 1 do
-        n.(i) <- count_neighbours b i size
-      done;
-    n
-
-let evolve n b i =
-    match b.(i) with
-      | Dead -> if n.(i) = 3 then Alive else Dead
-      | Alive -> 
-          match n.(i) with
-            | 2 | 3 -> Alive
-            | _ -> Dead      
-
-let next b size = 
-    let n = neighbours b size in
-      for i = 0 to (size * size)-1 do
-        b.(i) <- evolve n b i
-      done
-
-
-let rec mark_alive b size lst = 
-  match lst with
-    | [] -> ()
-    | h :: t -> b.(h) <- Alive; mark_alive b size t
+let is_alive idx b =
+  match b.(idx) with
+  | Alive -> true
+  | Dead -> false
 ;;
 
-(* form list of cells which will be marked alive. It is going to be middle row of the table 
-  example for size=8 output list = [24; 25; 26; 27; 28; 29; 30; 31];
+(* count for each cell which are alive *)
+let count_if_alive b x y size =
+  let i = get_index (x, y) size in
+  match i with
+  | x when -1 < x && x < size * size -> if is_alive x b then 1 else 0
+  | _ -> 0
+;;
+
+let get_position_coordinate x y =
+  [ x - 1, y - 1
+  ; x, y - 1
+  ; x + 1, y - 1
+  ; x - 1, y
+  ; (*(x, y);*)
+    x + 1, y
+  ; x - 1, y + 1
+  ; x, y + 1
+  ; x + 1, y + 1
+  ]
+;;
+
+let count_neighbours_cell b x y size =
+  let rec count_neighbours_cell_aux acc b lst size =
+    match lst with
+    | [] -> acc
+    | (x, y) :: tl ->
+      count_neighbours_cell_aux (acc + count_if_alive b x y size) b tl size
+  in
+  count_neighbours_cell_aux 0 b (get_position_coordinate x y) size
+;;
+
+let count_neighbours b i size =
+  let x, y = get_position i size in
+  count_neighbours_cell b x y size
+;;
+
+(* count the neighbours each cell has. edges and corners will have less neighbours *)
+let neighbours b size =
+  let n = Array.make (size * size) 0 in
+  for i = 0 to (size * size) - 1 do
+    n.(i) <- count_neighbours b i size
+  done;
+  n
+;;
+
+let evolve n b i =
+  match b.(i) with
+  | Dead -> if n.(i) = 3 then Alive else Dead
+  | Alive ->
+    (match n.(i) with
+     | 2 | 3 -> Alive
+     | _ -> Dead)
+;;
+
+let next b size =
+  let n = neighbours b size in
+  for i = 0 to (size * size) - 1 do
+    b.(i) <- evolve n b i
+  done
+;;
+
+let rec mark_alive b size lst =
+  match lst with
+  | [] -> ()
+  | h :: t ->
+    b.(h) <- Alive;
+    mark_alive b size t
+;;
+
+(* form list of cells which will be marked alive. It is going to be middle row of the table
+   example for size=8 output list = [24; 25; 26; 27; 28; 29; 30; 31];
 *)
 let form_list size =
   let l = ref [] in
-    for i = ((size / 2) * size) to ((size / 2) * size + size)-1 do
-      l := i :: !l;
-    done;
+  for i = size / 2 * size to (size / 2 * size) + size - 1 do
+    l := i :: !l
+  done;
   !l
+;;
